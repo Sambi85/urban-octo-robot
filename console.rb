91de695a321b76fec75d97e6b8652a33bc3f945a
Sam Lesser <59119626+Sambi85@users.noreply.github.com>
2020-07-22 13:56:46 -0400

    Add files via upload
    OO restaurant, recipe build out.
    Version 0.1

---
+++ console.rb
@@ -0,0 +1,18 @@
+require_relative "./recipes.rb"
+require_relative "./restaurant.rb"
+require 'pry'
+
+#Recipes
+recipe1 = Recipe.new("vegan chili","sweet & spicy")
+recipe2 = Recipe.new("hunter's stew", "default sunday special")
+recipe3 = Recipe.new("Fish Fry","use for the lunch crowds")
+
+#Restaurants
+restaurant1 = Restaurant.new("Sushi_Town",3)
+restaurant2 = Restaurant.new("Sarah's",4)
+restaurant3 = Restaurant.new("Dive Bar & Grille",3)
+
+
+#### DO NOT REMOVE #####
+binding.pry
+'wwwwwwww'
